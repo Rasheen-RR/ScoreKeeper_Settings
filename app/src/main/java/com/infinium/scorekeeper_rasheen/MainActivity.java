90cef66a875881f20b37241b67cf683d9342ad22
Rasheen <sheen.ruwisha@gmail.com>
2020-10-07 01:09:27 +0530

    New: Created scorekeeper application

---
+++ app/src/main/java/com/infinium/scorekeeper_rasheen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.infinium.scorekeeper_rasheen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
